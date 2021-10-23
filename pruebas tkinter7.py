e07bdd85b453ea89fcacac3bd853427520769011
JahazielHernandezHoyos <48532611+JahazielHernandezHoyos@users.noreply.github.com>
2021-10-23 17:22:52 -0500

    Delete pruebas tkinter7.py

--- pruebas tkinter7.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-
-root = Tk()
-lbred = Label(root,text="Red",fg="Red",relief=GROOVE)
-lbred.pack(fill=X)
-lbblue = Label(root,text="azul",fg="blue",relief=GROOVE)
-lbblue.pack(fill=Y)         # El control llena el control desocupado en la dirección horizontal
-root.mainloop()
